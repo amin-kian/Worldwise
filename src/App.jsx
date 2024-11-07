199d4970006a625a4d0b7ba724923cca7b41539e
amin-kian <a.kianfard80@gmail.com>
2024-11-07 22:44:13 +0330

    show cities

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
 import Homepage from "./pages/Homepage";
 import Login from "./pages/Login";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import AppLayout from "./pages/AppLayout";
 import PageNotFound from "./pages/PageNotFound";
+import CityList from "./components/CityList";
+
+const BASE_URL = "http://localhost:9000";
 
 function App() {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch (err) {
+        console.log(err);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
@@ -32,7 +55,12 @@ function App() {
           <Route
             index
             path="cities"
-            element={<p>Cities</p>}
+            element={
+              <CityList
+                cities={cities}
+                isLoading={isLoading}
+              />
+            }
           />
           <Route
             path="countries"
