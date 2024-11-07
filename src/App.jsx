8896340637cdfdf3fcca177a3b47368735a73d2b
amin-kian <a.kianfard80@gmail.com>
2024-11-07 20:52:48 +0330

    make pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+
 function App() {
-  return <div>Hello World</div>;
+  return <Homepage />;
 }
 
 export default App;
