6b1dd3a6ebb2101ed9951c360ebf341ccddea75b
amin-kian <a.kianfard80@gmail.com>
2024-11-07 21:19:54 +0330

    finish navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,32 @@
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
+import Login from "./pages/Login";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
 
 function App() {
-  return <Homepage />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={<Homepage />}
+        />
+        <Route
+          path="/login"
+          element={<Login />}
+        />
+        <Route
+          path="/product"
+          element={<Product />}
+        />
+        <Route
+          path="/pricing"
+          element={<Pricing />}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
